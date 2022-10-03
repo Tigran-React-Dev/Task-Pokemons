7e179f594f919cb5404693585ca67dfb8a01b648
Tigran-React-Dev <hambardzumyant351@gmail.com>
2022-10-03 23:16:17 +0400

    newbrench

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SearchBar from "./Components/SearchBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="Appp">
       <SearchBar />
       <Routes />
     </div>
